5375608ccc12ba76ac0041b5f6794aec17d2a272
zonareload <108830677+zonareload@users.noreply.github.com>
2022-07-22 01:56:23 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,23 +24,23 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "zona reload" //namabot kalian
+global.ownername= "zona reload" //nama kalian
+global.myweb ="https://zonareload.id" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/zonareload" //bebas asal jan hapus
+global.github = "https://zonarelod.github.io/" //bebas
+global.email = "zona.reload.indonesia@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium = ['62887435047326'] //premium user
-global.owner = ["622150996855","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.owner = ["6285397776608","6285398698742"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285397776608" // nomor wa kalian
+global.ownernomerr = "+6285397776608" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '©zona reload 2017' //sticker wm ubah
+global.author = 'Di Buat Oleh zona reload' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 
